a1ef0b3221110b487f542cc6f00034039acda05e
Moatasem10 <36310115+Moatasem10@users.noreply.github.com>
2018-02-22 02:13:01 +0200

    Add files via upload

---
+++ Individual work/Sheet-1/Multiple_LEDs.ino
@@ -0,0 +1,199 @@
+int Led1 = 13;
+int Led2 = 12;
+int Led3 = 11;
+int Led4 = 10;
+int Led5 =  9;
+int Led6 =  8;
+int Led7 =  7;
+int Led8 =  6;
+int Led9 =  5;
+int Leda =  4;
+int Ledb =  3;
+int Ledc =  2;
+
+void setup() 
+{
+    pinMode(Led1, OUTPUT);
+    pinMode(Led2, OUTPUT);
+    pinMode(Led3, OUTPUT);
+    pinMode(Led4, OUTPUT);
+    pinMode(Led5, OUTPUT);
+    pinMode(Led6, OUTPUT);
+    pinMode(Led7, OUTPUT);
+    pinMode(Led8, OUTPUT);
+    pinMode(Led9, OUTPUT);
+    pinMode(Leda, OUTPUT);
+    pinMode(Ledb, OUTPUT);
+    pinMode(Ledc, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(Led1, HIGH);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW);    
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2, HIGH);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3, HIGH);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4, HIGH);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5, HIGH);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6, HIGH);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7, HIGH);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8, HIGH);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9, HIGH);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda, HIGH);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb, HIGH);
+    digitalWrite(Ledc,  LOW); 
+    delay(1000);
+
+    digitalWrite(Led1,  LOW);
+    digitalWrite(Led2,  LOW);
+    digitalWrite(Led3,  LOW);
+    digitalWrite(Led4,  LOW);
+    digitalWrite(Led5,  LOW);
+    digitalWrite(Led6,  LOW);
+    digitalWrite(Led7,  LOW);
+    digitalWrite(Led8,  LOW);
+    digitalWrite(Led9,  LOW);
+    digitalWrite(Leda,  LOW);
+    digitalWrite(Ledb,  LOW);
+    digitalWrite(Ledc, HIGH); 
+    delay(1000);
+}
