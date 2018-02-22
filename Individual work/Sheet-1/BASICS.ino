bf84c5f3465d62cb7180be20dcf7c3da7df14f98
amrfouda <36259970+amrfouda@users.noreply.github.com>
2018-02-22 23:02:53 +0200

    Add files via upload

---
+++ Individual work/Sheet-1/BASICS.ino
@@ -0,0 +1,14 @@
+int LED =13; 
+
+void setup ()
+{
+  pinMode(led, OUTPUT); //initialization the digital pin as an output
+}
+void loop ()
+{
+  digitalWrite(LED, HIGH); //apply high voltage
+  delay(1000); //wait for a second
+  digitalWrite(LED, LOW); //apply low voltage
+  delay(1000); //wait for a second 
+}
+
