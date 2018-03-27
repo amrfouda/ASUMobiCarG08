7c82900d3febf6c90f47ccd4f17a2d9623d8d148
Mahmoud Magdy <mahmoudmagdi20@gmail.com>
2018-03-27 18:37:24 +0200

    Using functions of motion for basic car motion using bluetooth module and adding the code for avoiding obstcales using ultraSonic sensor

--- Arduino code/control.ino
+++ Arduino code/control.ino
@@ -4,7 +4,12 @@ const byte mlf = 50 ;
 const byte mlb = 51 ;
 const byte mrs = 2 ;
 const byte mls = 3 ;
+const byte trig = 9 ;
+const byte echo = 8 ;
+int action = 0 ;
 
+long duration ;
+int distance ;
 
 void forward()
 {
@@ -97,10 +102,93 @@ void stopp()
 
 void setup() 
 {
- 
+  pinMode(mrf , OUTPUT);
+  pinMode(mrb , OUTPUT);
+  pinMode(mrs , OUTPUT);
+  pinMode(mlf , OUTPUT);
+  pinMode(mlb , OUTPUT);
+  pinMode(mls , OUTPUT);
+  pinMode(trig , OUTPUT);
+  pinMode(echo , INPUT);
+  
+  Serial.begin(9600);
+  
 }
 
 void loop() 
 {
- 
+  digitalWrite(trig , LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig , HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig , LOW);
+
+  duration = pulseIn(echo , HIGH) ;
+  distance = duration * (0.034/2) ;
+  
+  if(Serial.available() > 0)
+  {
+    action = Serial.read();
+
+    if(action == 'F' && distance > 40)
+    {
+      forward();
+    }
+    else if(action == 'F' )
+    {
+      while(distance <= 40)
+      {
+        left();
+        
+        delay(500);
+        digitalWrite(trig , LOW);
+        delayMicroseconds(2);
+        digitalWrite(trig , HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trig , LOW);
+
+        duration = pulseIn(echo , HIGH) ;
+        distance = duration * (0.034/2) ;
+        
+      }
+      
+    }
+    else if(action == 'B')
+    {
+      backward();
+    }
+    else if(action == 'R')
+    {
+      right();
+    }
+    else if(action == 'L')
+    {
+      left();
+    }
+
+    else if(action == 'G')
+    {
+      forwardLeft();
+    }
+
+    else if(action == 'I')
+    {
+      forwardRight();
+    }
+
+    else if(action == 'H')
+    {
+      backwardLeft();
+    }
+    
+    else if(action == 'J')
+    {
+      backwardRight();
+    }
+    else
+    {
+      stopp();
+    }
+  }
+  
 }
