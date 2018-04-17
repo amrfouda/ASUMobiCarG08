b9ac9cda248b13d2da45dab097bf21093fd8a89c
HeshamHussein50 <36860723+HeshamHussein50@users.noreply.github.com>
2018-04-18 01:54:21 +0200

    turning on and off a LED using bluetooth
    make the Arduino code for enabling the communication between the Arduino board and the smartphone. We will make a simple example, just turning on and off a LED

---
+++ Individual work/sketch_apr18a.ino
@@ -0,0 +1,22 @@
+#define ledPin 7
+int state = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(38400); // Default communication rate of the Bluetooth module
+}
+void loop() {
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+ }
+ if (state == '0') {
+  digitalWrite(ledPin, LOW); // Turn LED OFF
+  Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '1') {
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED: ON");;
+  state = 0;
+ } 
+}
