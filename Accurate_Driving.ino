b8a48bf5b30d61c2c6f5c883d9fe7d9d6396bc94
YoussefArnaouty <36207599+YoussefArnaouty@users.noreply.github.com>
2018-07-02 13:59:35 +0200

    Add files via upload

---
+++ Accurate_Driving.ino
@@ -0,0 +1,16 @@
+
+int acc_distance=0 ;                                 //the variable distance input from the user
+float time = (acc_distance)/(170/60*2*22/7*3.25);    //forward motion duration = distance required/(rps * wheel circumference)
+time = time *1000;                                   // time in milliseconds
+
+
+void loop() 
+{
+  if(acc_distance =/= 0)
+  { 
+    forward();      
+    delay(time);
+  }
+  else
+    stop();
+}
